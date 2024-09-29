3e40716150093fc7f3d2671d7d6b0e771d9056ac
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-09-29 18:41:53 +0530

    updated ports

--- node-user-management-app/server.js
+++ node-user-management-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = 9351;
+const port = 3001;
 const host = "localhost";
 const mongoose = require("mongoose");
 const router = require("./router");
